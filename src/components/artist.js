a45fc824691461941efb584bda2b3d910dbbebc9
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-07-26 13:03:18 +0100

    Refactoring of tests.

--- src/components/artist.js
+++ src/components/artist.js
@@ -1,6 +1,11 @@
 class Artist {
-  constructor(firstName) {
+  constructor(firstName, secondName, skill, charisma, ingenuity, era) {
     this.firstName = firstName;
+    this.secondName = secondName;
+    this.skill = skill;
+    this.charisma = charisma;
+    this.ingenuity = ingenuity;
+    this.era = era;
   }
 }
 
--- src/tests/artist.test.js
+++ src/tests/artist.test.js
@@ -1,8 +1,20 @@
 const Artist = require("../components/artist");
 
 describe("Artist", () => {
+  const artist = new Artist(
+    "Dita",
+    "Von Teese",
+    70,
+    80,
+    50,
+    "twentyfirst century"
+  );
+
   test("it returns the artist's first name", () => {
-    const artist = new Artist("Dita");
     expect(artist.firstName).toEqual("Dita");
+  });
+
+  test("it returns the artist's second name", () => {
+    expect(artist.secondName).toEqual("Von Teese");
   });
 });
